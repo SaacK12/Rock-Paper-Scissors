07fa742360bcd885a2f8afabdb1e3577db6afbe5
SaacK <161529566+SaacK12@users.noreply.github.com>
2025-06-22 14:11:14 -0300

    Add files via upload

--- logic game.js
+++ logic game.js
@@ -7,6 +7,10 @@ const indiceAleatorio= Math.floor(Math.random() * array.length)
 numeroElegido= array[indiceAleatorio];
 console.log("Numero elegido por la CPU: "+ numeroElegido)
 randomIcon()
+let imagenAlex= document.getElementById("Alex");
+    imagenAlex.src= "img/alex1.png";
+let imagenJanken= document.getElementById("Janken");
+    imagenJanken.src= "img/janken the great.png";
 }
 //
 function randomIcon(){
@@ -18,32 +22,26 @@ setInterval(() => {
   contador= (contador + 1) % imagenes.length; //Vuelve a 0 al final
 }, 500);
  }
- //
+ //Aca estoy seguro que me la re mande he hice cualquier cosa llamando a un monton de funciones,
+ //Podria de hecho crear solo tres funciones para las posibilidades de si perdes, empatas o ganas,
+ //Y capaz lo haga pero otro dia, encima esta escrito el nombre de las funciones en ingles para flashear que soy bilingue,
+ //Lo importante es que funciona y que se entiende la idea de lo que quiero hacer (creo)
 function rock(){
  switch (numeroElegido){
     case 1:
     changeIconToRock();
     randomIconToRock();
-    setTimeout(function(){
-    alert("Empate!");
-    start();
-    }, 300);
+    draw();
     break;
     case 2:
     changeIconToRock();
     randomIconToPaper();
-    setTimeout(function(){
-    alert("Perdiste! Te gano la CPU!");
-    start();
-    }, 300);
+    lose();
     break;
     case 3:
     changeIconToRock();
     randomIconToScissors();
-    setTimeout(function(){
-    alert("Ganaste! Derrotaste a la CPU!");
-    start();
-    }, 300);
+    win();
     break;
  }
 }
@@ -52,26 +50,17 @@ function paper(){
     case 1:
     changeIconToPaper();
     randomIconToRock();
-    setTimeout(function(){
-    alert("Ganaste! Derrotaste a la CPU!")
-    start()
-    }, 300);
+    win();
     break;
     case 2:
     changeIconToPaper();
     randomIconToPaper();
-    setTimeout(function(){
-    alert("Empate!")
-    start()
-    }, 300);
+    draw();
     break;
     case 3:
     changeIconToPaper();
     randomIconToScissors();
-    setTimeout(function(){
-    alert("Perdiste! Te gano la CPU!")
-    start()
-    }, 300);
+    lose()
     break;
  }
 }
@@ -80,29 +69,52 @@ switch (numeroElegido){
     case 1:
     changeIconToScissors();
     randomIconToRock();
-    setTimeout(function(){
-    alert("Perdiste! Te gano la CPU!")
-    start()
-    }, 300);
+    lose();
     break;
     case 2:
     changeIconToScissors();
     randomIconToPaper();
-    setTimeout(function(){
-    alert("Ganaste! Derrotaste a la CPU!")
-    start()
-    }, 300);
+    win()
     break;
     case 3:
     changeIconToScissors();
     randomIconToScissors();
-    setTimeout(function(){
-    alert("Empate!")
-    start()
-    }, 300);
+    lose();
     break;
  }
 }
+
+function win(){
+let imagenAlex= document.getElementById("Alex");
+    imagenAlex.src= "img/alexWin.png";
+let imagenJanken= document.getElementById("Janken");
+    imagenJanken.src= "img/jankenLose.png";
+setTimeout(function(){
+    alert("Ganaste! Derrotaste a la CPU!");
+    start();
+    }, 300);
+}
+function lose(){
+let imagenAlex= document.getElementById("Alex");
+    imagenAlex.src= "img/alexLose.png";
+let imagenJanken= document.getElementById("Janken")
+    imagenJanken.src= "img/jankenWin.png"
+setTimeout(function(){
+    alert("Perdiste! Te gano la CPU!");
+    start();
+    }, 300);
+}
+function draw(){
+let imagenAlex= document.getElementById("Alex")
+    imagenAlex.src= "img/alexLose.png";
+let imagenJanken= document.getElementById("Janken")
+    imagenJanken.src= "img/JankenLose.png"
+setTimeout(function(){
+    alert("Empate!");
+    start();
+    }, 300);
+}
+
 function changeIconToRock(){
 let imagen= document.getElementById("playerChoice");
     imagen.src= "img/Rock.png";
@@ -127,3 +139,4 @@ function randomIconToScissors(){
 let imagen= document.getElementById("randomChoice")
     imagen.src= "img/scissors.png"
 }
+//Es que mira cuantas funciones hice XD
